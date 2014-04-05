625aadf3e4235335dcdb530a95d61580cf26f5da
aluno <aluno@localhost>
2014-04-05 21:45:19 +0000

    Servlet implementado

--- src/java/br/edu/uninassau/provaColegiada/curso/controller/CursoListarController.java
+++ src/java/br/edu/uninassau/provaColegiada/curso/controller/CursoListarController.java
@@ -6,7 +6,12 @@
 
 package br.edu.uninassau.provaColegiada.curso.controller;
 
+import br.edu.uninassau.provaColegiada.curso.dao.CursoDAO;
+import br.edu.uninassau.provaColegiada.curso.Curso;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,5 +41,29 @@ public class CursoListarController extends HttpServlet
     {
         
     }     
+            CursoDAO dao = (CursoDAO) request.getAttribute( "CursoDAO" ); // recupera o DAO
+            {
+}
+        if( request.getSession().getAttribute( "curso_msg" ) != null )
+        {
+            String msg = (String) request.getSession().getAttribute( "curso_msg" );
+            request.getSession().removeAttribute( "curso_msg" );
             
-            }
+            request.setAttribute( "msg" , msg );
+}
+        
+        try
+        {
+            List<Curso> lista = dao.listar();
+            request.setAttribute( "lista" , lista );        // manda a lista para o JSP
+            
+            // Exibi a pagina para login
+            RequestDispatcher rd = request.getRequestDispatcher( "/curso/listar.jsp" );
+            rd.forward( request , response );
+        }
+        catch( Exception err )
+        {
+            throw new ServletException( err );
+        }
+    }
+
